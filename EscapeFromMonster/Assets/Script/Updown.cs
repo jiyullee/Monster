4bbee00456d0771d4dbf529efaaf2e0a21373cb5
seuheo <1215tngh@naver.com>
2019-07-24 20:42:49 +0900

    Add AI

--- EscapeFromMonster/Assets/Script/Updown.cs
+++ EscapeFromMonster/Assets/Script/Updown.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Updown : MonoBehaviour
 {
-    int forwardSpeed, backSpeed, rightSpeed, leftSpeed;
+    int forwardSpeed=5, backSpeed=3, rightSpeed=4, leftSpeed=4;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,19 +16,19 @@ public class Updown : MonoBehaviour
     {
 
         if (Input.GetKey(KeyCode.W)) {
-            transform.Translate(Vector3.forward * forwardSpeed);
+            transform.Translate(Vector3.forward * forwardSpeed *Time.deltaTime);
         }
         if (Input.GetKey(KeyCode.S))
         {
-            transform.Translate(Vector3.back * backSpeed);
+            transform.Translate(Vector3.back * backSpeed * Time.deltaTime);
         }
         if (Input.GetKey(KeyCode.D))
         {
-            transform.Translate(Vector3.right * rightSpeed);
+            transform.Translate(Vector3.right * rightSpeed * Time.deltaTime);
         }
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Translate(Vector3.right * leftSpeed);
+            transform.Translate(Vector3.left * leftSpeed * Time.deltaTime);
         }
     }
 }
