d306b20e01713fb12c6b57bcdbdba235c5b2df3a
seuheo <1215tngh@naver.com>
2019-07-24 19:15:53 +0900

    Add player

---
+++ EscapeFromMonster/Assets/Script/Updown.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Updown : MonoBehaviour
+{
+    int forwardSpeed, backSpeed, rightSpeed, leftSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKey(KeyCode.W)) {
+            transform.Translate(Vector3.forward * forwardSpeed);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.back * backSpeed);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * rightSpeed);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.right * leftSpeed);
+        }
+    }
+}
