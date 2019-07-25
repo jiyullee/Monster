799ff9fb30a54fc770384d6211987eade798706e
seuheo <1215tngh@naver.com>
2019-07-25 18:46:59 +0900

    Add UI

---
+++ EscapeFromMonster/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    int rotSpeed = 10;
+    int forwardSpeed = 5;
+    int sideSpeed = 4;
+    int backSpeed = 3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float MouseX = Input.GetAxis("Mouse X");
+        
+        transform.Rotate(Vector3.up * rotSpeed * MouseX, Space.World);
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.forward * forwardSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.left * sideSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.left * -sideSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.back * backSpeed * Time.deltaTime);
+        }
+
+    }
+}
