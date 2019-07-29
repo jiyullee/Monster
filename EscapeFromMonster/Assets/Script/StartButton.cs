ba8dd305e31b2b2c9003ce3be5d589cb56f2bb69
seuheo <1215tngh@naver.com>
2019-07-29 20:18:22 +0900

    Add Monster Ani

---
+++ EscapeFromMonster/Assets/Script/StartButton.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartButton : MonoBehaviour
+{
+    public void GameStart(GameObject Loading)
+    {
+        SceneManager.LoadScene("SampleScene");
+        Loading.SetActive(true);
+    }
+}
