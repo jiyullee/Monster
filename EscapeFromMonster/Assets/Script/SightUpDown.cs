b52d6b0dd50bd95074087258f5c17dd02343f3df
jedy100 <jedy100@naver.com>
2019-07-29 19:19:19 +0900

    Make AI

--- EscapeFromMonster/Assets/Script/SightUpDown.cs
+++ EscapeFromMonster/Assets/Script/SightUpDown.cs
@@ -7,16 +7,16 @@ public class SightUpDown : MonoBehaviour
     int rotSpeed = 10;
     void Update()
     {
-        float MouseY = Input.GetAxis("Mouse Y");
-        print(transform.rotation);
-        if (0.3f >= transform.rotation.x && transform.rotation.x >= -0.5f)
-        {
-            transform.Rotate(new Vector3(-1, 0, 0) * rotSpeed * MouseY);
-        }
-        else if (transform.rotation.x < -0.5f)
-            transform.Rotate(new Vector3(-0.5f, 0, 0));
-        else if (transform.rotation.x > 0.3f)
-            transform.Rotate(new Vector3(0.3f, 0, 0));
+        //float MouseY = Input.GetAxis("Mouse Y");
+        //print(transform.rotation);
+        //if (0.3f >= transform.rotation.x && transform.rotation.x >= -0.5f)
+        //{
+        //    transform.Rotate(new Vector3(-1, 0, 0) * rotSpeed * MouseY);
+        //}
+        //else if (transform.rotation.x < -0.5f)
+        //    transform.rotation= Quaternion.Euler(-0.5f, 0, 0);
+        //else if (transform.rotation.x > 0.3f)
+        //    transform.rotation = Quaternion.Euler(0.3f, 0, 0);
 
     }
 }
