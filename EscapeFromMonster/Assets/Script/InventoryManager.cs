aaca3aa9ce73b7ec186d119f5426647469611a9d
jiyullee <syk_09@naver.com>
2019-07-30 20:15:51 +0900

    init

---
+++ EscapeFromMonster/Assets/Script/InventoryManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Service;
+
+public class InventoryManager : Singleton<InventoryManager>
+{
+    public GameObject[] inGameSlot;
+    GameObject[] slot = new GameObject[9];
+    static int i = 0;
+    bool isFull = false;
+    public void InputItem(GameObject item)
+    {
+        while (i < inGameSlot.Length && isFull == false)
+        {
+            if (slot[i] == null)
+            {
+                slot[i] = item;
+                inGameSlot[i].GetComponent<UseItem>().item = item;
+                inGameSlot[i].GetComponent<Image>().sprite = item.GetComponent<ItemManager>().itemImage;
+                break;
+            }
+
+            i++;
+            
+        }
+        if (i == inGameSlot.Length)
+        {
+            isFull = true;
+        }
+            
+    }  
+    
+}
---
+++ EscapeFromMonster/Assets/Script/UseItem.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class UseItem : MonoBehaviour
+{
+    public GameObject item;
+    Sprite slotImage;
+    private void Start()
+    {
+        slotImage = GetComponent<Image>().sprite;
+    }
+    public void Onclick()
+    {
+        if(item != null)
+        {
+            item.GetComponent<ItemManager>().Function();
+            GetComponent<Image>().sprite = slotImage;
+        }
+              
+    }
+}
